94b2d2f26b5b563e921c294ee99aa17096232157
Shawn Wallace <shawn@the-wallaces.net>
2017-12-14 17:46:28 -0500

    Added estimate models

---
+++ Estimator.entities/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Estimator.entities
+{
+  public abstract class EntityBase<TKey>
+  {
+    public TKey Id { get; set; }
+
+    public DateTime WhenCreated { get; set; } = DateTime.UtcNow;
+
+    public DateTime WhenLastModified { get; set; }
+  }
+}
---
+++ Estimator.entities/TeamRole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Estimator.entities
+{
+  public class TeamRole : EntityBase<Guid>
+  {
+    public string Name { get; set; }
+    public double Rate { get; set; }
+  }
+
+  public class Estimate : EntityBase<Guid>
+  {
+    public string Title { get; set; }
+    public string Client { get; set; }
+
+    public virtual List<TeamRole> TeamRoles { get; set; }
+    public virtual List<Iteration> Iterations { get; set; }
+  }
+
+  public class Iteration : EntityBase<Guid>
+  {
+    public string Label { get; set; }
+    public int Hours { get; set; }
+    public int TargetVelocity { get; set; }
+    public int DisplayOrder { get; set; }
+
+    public virtual List<TeamRole> IterationRoles { get; set; }
+  }
+}
